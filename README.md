d12214df73f736619ea48dcff8d799b3abb7fe21
rayan <rayan13170@protonmail.com>
2024-02-19 21:24:09 +0100

    added Readme

---
+++ README.md
@@ -0,0 +1,9 @@
+# Minimalist Web Server
+
+The Minimalist Web Server is a lightweight Python-based web server utilizing the socket module. While simplistic, it offers the functionality to serve static files. This project is intended for educational purposes rather than production use.
+
+## Why ?
+
+The goal of creating this web server was to explore how web servers function and gain a better understanding of the HTTP protocol.By constructing a basic server from scratch, the aim was to grasp fundamental concepts such as handling incoming connections, parsing HTTP requests, and serving responses.
+
+This project serves as a stepping stone to comprehend the workings of sophisticated web servers like Apache or Nginx.
--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 import socket
 
 
-class MinimalistServer:
-    def __init__(self, host="127.0.0.1", port=8000) -> None:
-        # Define the host and port
+class MinimalistWebServer:
+    def __init__(self, host="127.0.0.1", port=8080) -> None:
+        """
+        Function to initialize the web server class.
 
-        self.HOST = host
-        self.PORT = port
+        We create a TCP socket listening for the initial
+        client connexion through IPv4.
+
+        Then we bind our socket to our host:port to be able to receive incoming
+        connections on this address.
+
+        After binding, we use server_socket.listen() to prepare the server to accept
+        connections.
+
+        """
+
+        # Define the host and port
+        self.HOST: str = host
+        self.PORT: int = port
 
         # Define the response
         self.response = """HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<html><body><h1>Hello, world!</h1></body></html>"""
@@ -36,12 +49,14 @@ class MinimalistServer:
                 finally:
                     # Close the client socket
                     client_socket.close()
+
+        # Gracefully catch KeyboardInterrupt exception
         except KeyboardInterrupt:
             print("Server shutting down...")
             # Gracefully shutting down server socket
             self.server_socket.close()
 
 
-server = MinimalistServer()
+server = MinimalistWebServer()
 
 server.listen_forever()
