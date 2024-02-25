00df8473d671caa720f9fbe3d92276dfbfde32a0
rayan <rayan13170@protonmail.com>
2024-02-25 12:05:03 +0100

    added more tests

--- tests.py
+++ tests.py
@@ -1,9 +1,28 @@
 import requests
 import time
+import pytest
 import threading
 
 from MinimalistServer import MinimalistWebServer
 from BaseServer.BaseServer import BaseServer
+
+
+@pytest.fixture(scope="module")
+def web_server():
+    """Fixture to start and stop the server for the tests.
+    Same as a setUp and teardown method in unittest."""
+    server = MinimalistWebServer()
+
+    server_thread = threading.Thread(target=server.listen_forever)
+    server_thread.daemon = True
+    server_thread.start()
+    time.sleep(1)
+
+    yield server
+
+    server.stop()
+    server_thread.join(timeout=2)
+
 
 # HOST AND PORT
 
@@ -32,18 +51,46 @@ def test_custom_host_is_taken():
 
 # RESPONSES
 
-def test_default_response_is_correct():
-    server = MinimalistWebServer()
 
-    server_thread = threading.Thread(target=server.listen_forever)
-    server_thread.daemon = True
-    server_thread.start()
-    time.sleep(1)
+def test_default_response_is_correct(web_server):
     # Fetch the default response
     response = requests.get("http://127.0.0.1:8080")
     assert response.status_code == 200
 
-    # Stop the server
-    server.stop()
-    server_thread.join(timeout=2)
 
+def test_404_is_returned_on_bad_url(web_server):
+    response = requests.get("http://127.0.0.1:8080/not_existing_path")
+    assert response.status_code == 404
+
+
+def test_405_is_returned_on_bad_method(web_server):
+    response = requests.post("http://127.0.0.1:8080")
+    assert response.status_code == 405
+
+
+def test_default_page_content_is_correct(web_server):
+    response = requests.get("http://127.0.0.1:8080")
+    assert "A Minimalist Web Server" in response.text
+
+
+# BaseServer
+
+
+def test_non_int_port_raises_error():
+    with pytest.raises(ValueError):
+        BaseServer(port="non_int", host="127.0.0.1")
+
+
+def test_non_string_host_raises_error():
+    with pytest.raises(ValueError):
+        BaseServer(port=8080, host=123)
+
+
+def test_host_is_not_valid_ip_raises_error():
+    with pytest.raises(ValueError):
+        BaseServer(port=8080, host="not_valid_ip")
+
+
+def test_port_out_of_range_raises_error():
+    with pytest.raises(ValueError):
+        BaseServer(port=70000, host="127.0.0.1")
